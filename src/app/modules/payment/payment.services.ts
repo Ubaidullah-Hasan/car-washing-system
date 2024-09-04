e2a1b210d6d3d743f37ab70edc7f7173cd22de81
Ubaidullah-Hasan <hm2964133@gmail.com>
2024-09-04 18:49:03 +0600

    fail page design

--- src/app/modules/payment/payment.services.ts
+++ src/app/modules/payment/payment.services.ts
@@ -3,9 +3,17 @@ import { readFileSync } from "fs";
 
 
 const confirmationService = async (transactionId: string, status: string) => {
-    const filePath = path.join(__dirname, "../../../views/success.html")
-    let template = readFileSync(filePath, "utf-8");
+    let filePath;
+
     
+    if(status === "success"){
+        filePath = path.join(__dirname, "../../../views/success.html");
+    } else if(status === "fail") {
+        filePath = path.join(__dirname, "../../../views/fail.html");
+    }
+
+    const template = readFileSync(filePath!, "utf-8");
+
     return template;
 }
 
